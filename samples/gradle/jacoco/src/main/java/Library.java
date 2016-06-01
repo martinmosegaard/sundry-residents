edc128d4829c66d2f57be5ba12abf9d28ea88ed9
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-06-01 09:52:29 +0200

    Add Gradle JaCoCo sample

---
+++ samples/gradle/jacoco/src/main/java/Library.java
@@ -0,0 +1,15 @@
+public class Library {
+
+  public boolean someLibraryMethod() {
+    return true;
+  }
+
+  public int someUntestedMethod(int n) {
+    if (n == 0) {
+      return n;
+    } else {
+      return n * n;
+    }
+  }
+
+}
