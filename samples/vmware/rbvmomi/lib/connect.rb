80cb6189d30e1a458d90cfa1d60dd0617fe5439b
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-08-19 10:56:53 +0200

    Ease up on the comments

--- samples/vmware/rbvmomi/lib/connect.rb
+++ samples/vmware/rbvmomi/lib/connect.rb
@@ -2,8 +2,8 @@ require 'rbvmomi'
 
 # Connect to a vCenter Server or a standalone ESX host.
 class Connect
-  # Connect to an ESX or vCenter host and return the service instance
-  # used to browse the inventory of managed objects.
+  # Connect and return the service instance used to
+  # browse the inventory of managed objects.
   def self.login(host, user, password)
     vim = RbVmomi::VIM.connect host: host, user: user, password: password, insecure: true
     vim.serviceInstance
